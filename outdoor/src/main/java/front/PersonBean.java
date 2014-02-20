c5915c206e14a483b7295d5f0a697dd4fa366667
Jerome <jerome.desachy@free.fr>
2014-02-20 20:35:35 +0100

    Suppression de l'index menu

--- outdoor/src/main/java/front/PersonBean.java
+++ outdoor/src/main/java/front/PersonBean.java
@@ -2,6 +2,11 @@ package front;
 
 import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
 public class PersonBean implements Serializable {
 
 	private static final long serialVersionUID = -7600290562999743296L;
