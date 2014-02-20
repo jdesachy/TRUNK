0c344e27e4affa20ad7c24327f2c4f93e8c8afbf
Jerome <jerome.desachy@free.fr>
2014-02-20 20:10:24 +0100

    Corrections

---
+++ outdoor/src/main/java/db/Person.java
@@ -0,0 +1,40 @@
+package db;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity
+public class Person {
+
+	@Id
+	private long id;
+	private String firstName;
+	private String lastName;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
