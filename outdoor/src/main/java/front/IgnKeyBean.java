9c9b100e600e08a9c2da537420fb6074411ed357
Jerome <jerome.desachy@free.fr>
2014-03-24 14:22:11 +0100

    Lecture carte IGN

--- outdoor/src/main/java/front/IgnKeyBean.java
+++ outdoor/src/main/java/front/IgnKeyBean.java
@@ -15,17 +15,31 @@ public class IgnKeyBean {
 
 	private final Logger log = Logger.getLogger(IgnKeyBean.class.getName());
 	private String value;
+	private String host;
+	private String protocol;
 
 	public IgnKeyBean() {
 		try {
 			this.value = Configuration.getInstance().getString("ign.key");
+			this.host = Configuration.getInstance().getString("ign.proxy.host");
+			this.protocol = Configuration.getInstance().getString(
+					"ign.proxy.protocol");
 		} catch (InitConfigurationException e) {
 			log.log(Level.WARNING,
-					"Impossible de rŽcupŽrer la clŽ ign en configaration", e);
+					"Impossible de récupérer la clé ign en configaration", e);
 		}
 	}
 
 	public String getValue() {
 		return value;
 	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public String getProtocol() {
+		return protocol;
+	}
+
 }
